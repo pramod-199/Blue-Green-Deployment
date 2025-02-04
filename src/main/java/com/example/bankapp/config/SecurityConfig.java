196452a9b5085b4d2098f756b616a14b2bd947fa
pramod-199 <146347894+pramod-199@users.noreply.github.com>
2025-02-05 01:09:27 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();1
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
